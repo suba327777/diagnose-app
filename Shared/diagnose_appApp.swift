4637469358e48852addfcfe2d0da1d8e15037a05
suba327777 <subachav@gmail.com>
2022-04-15 14:16:49 +0900

    Initial Commit

---
+++ Shared/diagnose_appApp.swift
@@ -0,0 +1,20 @@
+//
+//  diagnose_appApp.swift
+//  Shared
+//
+//  Created by miuraken on R 4/04/15.
+//
+
+import SwiftUI
+
+@main
+struct diagnose_appApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
