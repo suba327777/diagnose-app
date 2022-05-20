fb2ac8669481ff95914d11b0119d4ec2ababf80f
suba327777 <subachav@gmail.com>
2022-05-20 18:11:54 +0900

    add read cdv file #4

--- Shared/QuestionView.swift
+++ Shared/QuestionView.swift
@@ -22,6 +22,32 @@ struct QuestionView: View {
         }
 }
 
+
+func readCSV(){
+    var csvArray=[String]()
+    var questionList = [[String]]()
+    // ファイルが存在するか
+    guard let path=Bundle.main.path(forResource: "dataList", ofType: "csv") else{
+        print("csvファイルはないよ！")
+        return
+    }
+    
+    do{
+        let csvString=try String(contentsOfFile:path,encoding: String.Encoding.utf8)
+        csvArray=csvString.components(separatedBy: .newlines)
+        csvArray.removeLast()
+    }catch let error as NSError{
+        print("エラー:\(error)")
+        return
+    }
+    
+    for questionData in csvArray {
+        let questionDetail = questionData.components(separatedBy: ",")
+        questionList.append(questionDetail)
+    }
+    print(questionList)
+}
+
 struct QuestionView_Previews: PreviewProvider {
     static var previews: some View {
         QuestionView()
