a47fa1bafe46a9745470289644942a4d56e6e877
suba327777 <subachav@gmail.com>
2022-07-05 18:38:33 +0900

    add ImageView

---
+++ Shared/Image/ImageView.swift
@@ -0,0 +1,25 @@
+//
+//  ImageView.swift
+//  diagnose-app (iOS)
+//
+//  Created by miuraken on R 4/07/05.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var image:ImageModel
+    var body: some View {
+        VStack{
+            Image(image.imageName)
+            Text(image.title)
+        }
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView(image:imageArray[1])
+            .previewLayout(.sizeThatFits)
+    }
+}
