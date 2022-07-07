652fb491fc53b827c3cfa8beb80866130db588a1
suba327777 <subachav@gmail.com>
2022-07-07 17:25:03 +0900

    remove image info

--- Shared/Image/ImageView.swift
+++ Shared/Image/ImageView.swift
@@ -8,39 +8,39 @@
 import SwiftUI
 
 struct ImageView: View {
+    
     var image:ImageModel
+    
     var body: some View {
         VStack{
 //            順位を表示しその順位に応じた王冠を表示する
+
             Text(image.title)
 //                .font(.custom("Times-Roman", size: 30))
-            HStack{
-                Image(image.imageName)
-                    .resizable()
-                    .frame(width: 370, height: 200,alignment:.center)
-                    .cornerRadius(10)
-                VStack{
-                    Text(image.subTitle)
+
+            Image(image.imageName)
+                .resizable()
+                .frame(width: 400, height: 250,alignment:.center)
+                .cornerRadius(10)
+            
+            Text(image.subTitle)
 //                        .font(.custom("Times-Roman", size: 17))
-                        .padding()
-                    Text(image.info)
-//                        .font(.custom("Times-Roman", size: 18))
-                        .padding(.horizontal)
-                        .padding(.top)
-                }
-               
-                    
-            }
             .padding()
 //            .border(Color.black,width: 1)
         }
-
+        .onTapGesture {
+            guard let laboratoryURL = URL(string: image.url)
+             else { fatalError("Expected a valid URL") }
+            UIApplication.shared.open(laboratoryURL)
+        }
     }
 }
+
 
 struct ImageView_Previews: PreviewProvider {
     static var previews: some View {
         ImageView(image:imageArray[1])
             .previewLayout(.sizeThatFits)
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
