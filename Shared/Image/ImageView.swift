416bd3e68b03be289f559e61da0641dc8f406085
suba327777 <subachav@gmail.com>
2022-07-07 14:01:40 +0900

    add image Layout

--- Shared/Image/ImageView.swift
+++ Shared/Image/ImageView.swift
@@ -11,10 +11,30 @@ struct ImageView: View {
     var image:ImageModel
     var body: some View {
         VStack{
-            Image(image.imageName)
+//            順位を表示しその順位に応じた王冠を表示する
             Text(image.title)
-            Text(image.info)
+//                .font(.custom("Times-Roman", size: 30))
+            HStack{
+                Image(image.imageName)
+                    .resizable()
+                    .frame(width: 370, height: 200,alignment:.center)
+                    .cornerRadius(10)
+                VStack{
+                    Text(image.subTitle)
+//                        .font(.custom("Times-Roman", size: 17))
+                        .padding()
+                    Text(image.info)
+//                        .font(.custom("Times-Roman", size: 18))
+                        .padding(.horizontal)
+                        .padding(.top)
+                }
+               
+                    
+            }
+            .padding()
+//            .border(Color.black,width: 1)
         }
+
     }
 }
 
